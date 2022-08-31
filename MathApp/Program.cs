255b0bc56fee958d526fe820a342113e9a9342c6
BuildTools <unconfigured@null.spigotmc.org>
2022-08-31 14:35:30 -0500

    Add project files.

---
+++ MathApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace MathApp
+{
+    class Program
+    {
+
+        static void Main()
+        {
+            int a = 5, b = 3;
+            int sum = a + b;
+            int diff = a - b;
+            int mult = a * b;
+            int div = a/b;
+            Console.WriteLine("a is: " + a);
+            Console.WriteLine("b is: " + b);
+            Console.WriteLine("Sum: " + sum);
+            Console.WriteLine("Difference: " + diff);
+            Console.WriteLine("Product: " + mult);
+            Console.WriteLine("Quotient: " + div);
+        }
+    }
+}
+
